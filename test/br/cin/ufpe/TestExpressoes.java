288325ba163890939eebd031ff7aee738f7e7edc
Vitor Maciel <vitormf@yahoo.com>
2012-04-14 17:33:04 -0300

    Novos testes

--- test/br/cin/ufpe/TestExpressoes.java
+++ test/br/cin/ufpe/TestExpressoes.java
@@ -27,6 +27,7 @@ public class TestExpressoes {
 	@Test
 	public void numeroNegativo() throws RecognitionException {
 		assertEquals(-5.0, calcular("-5"));
+		assertEquals(5.0, calcular("-(-5)"));
 	}
 
 	@Test
@@ -62,7 +63,7 @@ public class TestExpressoes {
 	}
 
 	@Test
-	public void comparacoes() throws RecognitionException {
+	public void comparacaoBooleanos() throws RecognitionException {
 		assertEquals(true, calcular("verdadeiro==verdadeiro"));
 		assertEquals(true, calcular("falso==falso"));
 		assertEquals(true, calcular("verdadeiro!=falso"));
@@ -71,15 +72,52 @@ public class TestExpressoes {
 		assertEquals(false, calcular("falso!=falso"));
 		assertEquals(false, calcular("verdadeiro==falso"));
 		assertEquals(false, calcular("falso==verdadeiro"));
-		assertEquals(true, calcular(" 5>=4"));
-		assertEquals(true, calcular(" 5>=4"));
-		assertEquals(false, calcular(" 3>4"));
-		assertEquals(true, calcular(" 5.4 < 6"));
-		assertEquals(false, calcular(" 10<=9"));
-		assertEquals(true, calcular(" 25==25.0"));
-		assertEquals(true, calcular("1!=0"));
+	}
+	
+	@Test
+	public void comparacaoStrings() throws RecognitionException {
 		assertEquals(true, calcular("'texto teste'=='texto teste'"));
 		assertEquals(false, calcular("'texto teste 1'=='texto teste'"));
+	}
+	
+	@Test
+	public void comparacaoNumeros() throws RecognitionException {
+		
+		assertEquals(false,	calcular("5>=6"));
+		assertEquals(true,	calcular("5>=5"));
+		assertEquals(true,	calcular("5>=4"));
+		
+		assertEquals(false,	calcular("3>4"));
+		assertEquals(false,	calcular("3>3"));
+		assertEquals(true,	calcular("3>2"));
+		
+		assertEquals(true,	calcular("5<6"));
+		assertEquals(false,	calcular("5<5"));
+		assertEquals(false,	calcular("5<4"));
+		
+		assertEquals(true,	calcular("5<=6"));
+		assertEquals(true,	calcular("5<=5"));
+		assertEquals(false,	calcular("5<=4"));
+		
+		assertEquals(true,	calcular("5.4 < 6"));
+		
+		assertEquals(true,	calcular("25==25"));
+		assertEquals(false,	calcular("25==26"));
+		assertEquals(true,	calcular("25==25.0"));
+		assertEquals(true,	calcular("25.0==25"));
+		
+		assertEquals(true,	calcular("25!=26"));
+		assertEquals(false,	calcular("25!=25"));
+		assertEquals(false,	calcular("25!=25.0"));
+		assertEquals(false,	calcular("25.0!=25"));
+		
+		
+	}
+	
+	
+	
+	@Test
+	public void comparacaoMixed() throws RecognitionException {
 		assertEquals(false, calcular("'1'== 1"));
 		assertEquals(true, calcular("(((1+1)==3)==falso)"));
 		assertEquals(true, calcular("1+1==3==falso"));
