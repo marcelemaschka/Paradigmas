816251d0b526a7b5826ad4891b74228fe6d0c9f2
Thiago de Arruda <tpadilha84@gmail.com>
2012-04-16 14:31:19 -0300

    Sobrescrito o método padrao de tratamento de erro no parser/lexer e refatoraçao no teste que verifica uma comparacao mal formada

--- test/br/cin/ufpe/TestExpressoes.java
+++ test/br/cin/ufpe/TestExpressoes.java
@@ -1,9 +1,11 @@
 package br.cin.ufpe;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.MismatchedTokenException;
 import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
 
@@ -21,25 +23,22 @@ public class TestExpressoes {
 		escopo = new Escopo();
 		ANTLRStringStream cod = new ANTLRStringStream(codigo);
 		PortujavaLexer lexer = new PortujavaLexer(cod);
-		parser = new PortujavaParser(new CommonTokenStream(
-				lexer));
+		parser = new PortujavaParser(new CommonTokenStream(lexer));
 		Expressao exp = parser.expressao();
-		if(parser.getNumberOfSyntaxErrors()>0){
+		if (parser.getNumberOfSyntaxErrors() > 0) {
 			return null;
 		}
 		return exp.valor(escopo);
 	}
-	
-	
+
 	public void executar(String codigo) throws RecognitionException, Retorno {
 		escopo = new Escopo();
 		ANTLRStringStream cod = new ANTLRStringStream(codigo);
 		PortujavaLexer lexer = new PortujavaLexer(cod);
-		parser = new PortujavaParser(new CommonTokenStream(
-				lexer));
+		parser = new PortujavaParser(new CommonTokenStream(lexer));
 		Programa prog = parser.programa();
-		if(parser.getNumberOfSyntaxErrors()>0){
-			return ;
+		if (parser.getNumberOfSyntaxErrors() > 0) {
+			return;
 		}
 		prog.executar(escopo);
 	}
@@ -93,49 +92,46 @@ public class TestExpressoes {
 		assertEquals(false, calcular("verdadeiro==falso"));
 		assertEquals(false, calcular("falso==verdadeiro"));
 	}
-	
+
 	@Test
 	public void comparacaoStrings() throws RecognitionException {
 		assertEquals(true, calcular("'texto teste'=='texto teste'"));
 		assertEquals(false, calcular("'texto teste 1'=='texto teste'"));
 	}
-	
+
 	@Test
 	public void comparacaoNumeros() throws RecognitionException {
-		
-		assertEquals(false,	calcular("5>=6"));
-		assertEquals(true,	calcular("5>=5"));
-		assertEquals(true,	calcular("5>=4"));
-		
-		assertEquals(false,	calcular("3>4"));
-		assertEquals(false,	calcular("3>3"));
-		assertEquals(true,	calcular("3>2"));
-		
-		assertEquals(true,	calcular("5<6"));
-		assertEquals(false,	calcular("5<5"));
-		assertEquals(false,	calcular("5<4"));
-		
-		assertEquals(true,	calcular("5<=6"));
-		assertEquals(true,	calcular("5<=5"));
-		assertEquals(false,	calcular("5<=4"));
-		
-		assertEquals(true,	calcular("5.4 < 6"));
-		
-		assertEquals(true,	calcular("25==25"));
-		assertEquals(false,	calcular("25==26"));
-		assertEquals(true,	calcular("25==25.0"));
-		assertEquals(true,	calcular("25.0==25"));
-		
-		assertEquals(true,	calcular("25!=26"));
-		assertEquals(false,	calcular("25!=25"));
-		assertEquals(false,	calcular("25!=25.0"));
-		assertEquals(false,	calcular("25.0!=25"));
-		
-		
+
+		assertEquals(false, calcular("5>=6"));
+		assertEquals(true, calcular("5>=5"));
+		assertEquals(true, calcular("5>=4"));
+
+		assertEquals(false, calcular("3>4"));
+		assertEquals(false, calcular("3>3"));
+		assertEquals(true, calcular("3>2"));
+
+		assertEquals(true, calcular("5<6"));
+		assertEquals(false, calcular("5<5"));
+		assertEquals(false, calcular("5<4"));
+
+		assertEquals(true, calcular("5<=6"));
+		assertEquals(true, calcular("5<=5"));
+		assertEquals(false, calcular("5<=4"));
+
+		assertEquals(true, calcular("5.4 < 6"));
+
+		assertEquals(true, calcular("25==25"));
+		assertEquals(false, calcular("25==26"));
+		assertEquals(true, calcular("25==25.0"));
+		assertEquals(true, calcular("25.0==25"));
+
+		assertEquals(true, calcular("25!=26"));
+		assertEquals(false, calcular("25!=25"));
+		assertEquals(false, calcular("25!=25.0"));
+		assertEquals(false, calcular("25.0!=25"));
+
 	}
-	
-	
-	
+
 	@Test
 	public void comparacaoMixed() throws RecognitionException {
 		assertEquals(false, calcular("'1'== 1"));
@@ -144,16 +140,22 @@ public class TestExpressoes {
 		assertEquals(true, calcular("falso==verdadeiro==(1-2==3))"));
 		assertEquals(true, calcular("falso==falso==verdadeiro==falso==falso"));
 		assertEquals(false, calcular("1<2==(3>4)==falso==(12<=3*4)==falso"));
-		
+
 	}
-	
+
 	@Test
-	public void comparacaoMalFormada() throws RecognitionException, Retorno{
-		executar("x=0;se(3>2>1){x=1;}");
-		assertEquals(true, parser.getNumberOfSyntaxErrors()>0);
+	public void comparacaoMalFormada() throws RecognitionException, Retorno {
+		try {
+			executar("x=0;se(3>2>1){x=1;}");
+		} catch (RuntimeException e) {
+			assertTrue(e.getCause() instanceof MismatchedTokenException);
+		}
+	}
+
+	@Test
+	public void comparacaoECondicional() throws RecognitionException, Retorno {
 		executar("x=0;se(0==x==verdadeiro){x=1;}");
 		assertEquals(1L, escopo.get("x"));
-		
 	}
 
 	@Test
@@ -170,12 +172,11 @@ public class TestExpressoes {
 	public void numeroNegativoExprBinaria() throws RecognitionException {
 		assertEquals(-10.0, calcular("5 *    -2"));
 	}
-	
-	
+
 	@Test
-	public void expressaoUnaria() throws RecognitionException{
+	public void expressaoUnaria() throws RecognitionException {
 		assertEquals(false, calcular("5>1<<1*3"));
-		assertEquals(16, calcular( "128>>6/2"));
+		assertEquals(16, calcular("128>>6/2"));
 	}
 
 	@Test
@@ -187,8 +188,8 @@ public class TestExpressoes {
 	public void multiplicacao() throws RecognitionException {
 		assertEquals(21.0, calcular("4.2*5"));
 	}
-	
-	@Test(expected=UnsupportedOperationException.class)
+
+	@Test(expected = UnsupportedOperationException.class)
 	public void multiplicacaoString() throws RecognitionException {
 		assertEquals("testeteste", calcular("'teste'*2"));
 	}
